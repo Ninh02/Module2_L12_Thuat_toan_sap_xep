ebc528283fe19f4512ec8abf565f9430b08cf5c9
Ninh02 <ninhdo10021998@gmail.com>
2021-05-31 16:32:56 +0700

    sap xep chon

--- Sap_xep_chon/Sap_xep_chon.php
+++ Sap_xep_chon/Sap_xep_chon.php
@@ -1,1 +1,20 @@
 <?php
+function selectionSort($list){
+    for($i=0;$i<count($list);$i++){
+        $max=$i;
+        for($j=$i+1;$j<count($list);$j++){
+            if($list[$max]<$list[$j]){
+                $max=$j;
+            }
+        }
+        if($max!=$i){
+            $temp=$list[$max];
+            $list[$max]=$list[$i];
+            $list[$i]=$temp;
+        }
+    }
+    return $list;
+}
+$arr=[1, 9, 4.5, 6.6, 5.7, -4.5];
+echo "<pre>";
+print_r(selectionSort($arr));
